212c425a2721210eabc096d8c0979695cd101dde
Denis Barbier <bouzim@gmail.com>
2011-03-19 00:31:01 +0100

    Replace Abs(foo < bar) by Abs(foo) < bar
    This is legal C++, but certainly not the expected result,
    (foo < bar) is a boolean which is promoted to an int, and
    the function Abs(int) is called.
    
    These bugs have been reported by Fotios Sioutis on the
    opencascade forum:
      http://www.opencascade.org/org/forum/thread_20187/
    BugID=OCC22324

--- ros/src/Adaptor3d/Adaptor3d_CurveOnSurface.cxx
+++ ros/src/Adaptor3d/Adaptor3d_CurveOnSurface.cxx
@@ -133,7 +133,7 @@ static void Hunt(const TColStd_Array1OfReal& Arr,
   if(Abs(Coord-Arr(i))<Tol)
     Iloc = i;
   else
-    if(Abs(Coord-Arr(i)>Tol)) Standard_NotImplemented::Raise("Adaptor3d_CurveOnSurface:Hunt");
+    if(Abs(Coord-Arr(i))>Tol) Standard_NotImplemented::Raise("Adaptor3d_CurveOnSurface:Hunt");
 }
 
 //=======================================================================
