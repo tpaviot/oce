58a332df9d82951b5fcc064b568df5f87404f88e
Denis Barbier <bouzim@gmail.com>
2011-03-30 17:22:14 +0200

    Fix a bug introduced in "Fix build failures with Borland compiler"
    ViewerTest_ViewerCommands.cxx could not be compiled when WNT is not
    set.  I introduced this bug in 6b0b930 when editing Fotis' changes
    to make them more generic.
    
    This commit must be merged into 6b0b930 before submitting to upstream.

--- ros/src/ViewerTest/ViewerTest_ViewerCommands.cxx
+++ ros/src/ViewerTest/ViewerTest_ViewerCommands.cxx
@@ -977,22 +977,6 @@ int ViewerMainLoop(Standard_Integer argc, const char** argv)
 
 #else
 
-int min( int a, int b )
-{
-  if( a<b )
-    return a;
-  else
-    return b;
-}
-
-int max( int a, int b )
-{
-  if( a>b )
-    return a;
-  else
-    return b;
-}
-
 int ViewerMainLoop(Standard_Integer argc, const char** argv)
 
 { Standard_Boolean pick = argc > 0;
