d654d21db3f5d3fd5c06270359caa4f77b52ea92
QbProg <tholag@gmail.com>
2011-05-17 00:47:05 +0200

    Cleaned up previous commits: - removed pragmas - removed NOP cmake instructions

--- src/OpenGl/OpenGl_attri.cxx
+++ src/OpenGl/OpenGl_attri.cxx
@@ -107,9 +107,7 @@ used in immediat mode context.
   #include <gl2ps.h>
   /* OCC22216 NOTE: linker dependency can be switched off by undefining macro. 
      Pragma comment for gl2ps.lib is defined only here. */ 
-  #ifdef _MSC_VER 
-  //#pragma comment( lib, "gl2ps.lib" )
-  #endif
+  
   #ifdef __BORLANDC__
   #include <gl2ps.c>
   #endif
