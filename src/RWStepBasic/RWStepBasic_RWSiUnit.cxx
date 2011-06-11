ada0877433d6dc32cdef2478703ae3311f759b29
QbProg <tholag@gmail.com>
2011-06-11 16:59:55 +0200

    [warning-fix][uninitialized-var]
    Fixes (and todos) in TKStepBase

--- src/RWStepBasic/RWStepBasic_RWSiUnit.cxx
+++ src/RWStepBasic/RWStepBasic_RWSiUnit.cxx
@@ -67,7 +67,7 @@ void RWStepBasic_RWSiUnit::ReadStep(const Handle(StepData_StepReaderData)& data,
   data->CheckDerived(num,1,"dimensions",ach,Standard_False);
 
   // --- own field : prefix ---
-  StepBasic_SiPrefix aPrefix;
+  StepBasic_SiPrefix aPrefix = StepBasic_spExa; // (0)
   Standard_Boolean hasAprefix = Standard_False;
   if (data->IsParamDefined(num,2)) {
     if (data->ParamType(num,2) == Interface_ParamEnum) {
@@ -80,7 +80,7 @@ void RWStepBasic_RWSiUnit::ReadStep(const Handle(StepData_StepReaderData)& data,
   }
   
   // --- own field : name ---
-  StepBasic_SiUnitName aName;
+  StepBasic_SiUnitName aName = StepBasic_sunMetre; // 0
   if (data->ParamType(num,3) == Interface_ParamEnum) {
     Standard_CString text = data->ParamCValue(num,3);
     if(!DecodeName(aName,text))
@@ -88,6 +88,8 @@ void RWStepBasic_RWSiUnit::ReadStep(const Handle(StepData_StepReaderData)& data,
   }
   else
     ach->AddFail("Parameter #3 (name) is not an enumeration");
+
+  // @todo Apart the fail, nothing is done , and wrong enum values are used
 
   //--- Initialisation of the read entity ---
   ent->Init(hasAprefix, aPrefix, aName);
