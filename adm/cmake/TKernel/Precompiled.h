adf461234e3666a403ccd881f4a78f5f5e4034b8
QbProg <tholag@gmail.com>
2011-06-26 17:22:23 +0200

    Removed useless code from TKernel/Precompiled.h

--- adm/cmake/TKernel/Precompiled.h
+++ adm/cmake/TKernel/Precompiled.h
@@ -5,20 +5,3 @@
 #include <math.h>
 #include "Standard.hxx"
 #include "Standard_PrimitiveTypes.hxx"
-
-#if 0
-#undef _INC_WINDOWS
-
-
-#include "Standard_Macro.hxx"
-#include "Standard_TypeDef.hxx"
-
-
-#include "Standard_DefineHandle.hxx"
-#include "Standard_PrimitiveTypes.hxx"
-#include "Standard_Transient.hxx"
-
-#include "Standard_DefineException.hxx"
-#include "MMgt_TShared.hxx"
-
-#endif
