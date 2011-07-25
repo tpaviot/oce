tpaviot/oce

commit 82de4a413299ed72eeb528e18a34baa5fb194fe4

[warning-fix][unhandled-enumeration-value]
Fix these warnings by adding a default NULL behavior:
default: /* Do nothing */ break;

Enough for fixing warnings, some cases would certainly
require than an exception is raised.

src/DNaming/DNaming_TransformationDriver.cxx
@@ -314,6 +314,7 @@ static void CollectShapes(const TopoDS_Shape& SSh, TopoDS_Compound& C,
 //	  TagMap.Bind(SSh, aNS->Label().Tag());
       }
     break;
+  default: /* Do nothing */ break;
   }   
 }
 

src/Adaptor3d/Adaptor3d_HSurfaceTool.cxx
@@ -17,6 +17,7 @@ Standard_Integer Adaptor3d_HSurfaceTool::NbSamplesU(const Handle(Adaptor3d_HSurf
       return (nbs < 2 ? 2 : nbs);
     }
     case GeomAbs_Torus: return 20;
+	default: /* Do nothing */ break;
   }
   return 10;
 }
@@ -38,6 +39,7 @@ Standard_Integer Adaptor3d_HSurfaceTool::NbSamplesV(const Handle(Adaptor3d_HSurf
     case GeomAbs_Torus:
     case GeomAbs_SurfaceOfRevolution:
     case GeomAbs_SurfaceOfExtrusion: return 15;
+	default: /* Do nothing */ break;
   }
   return 10;
 }

src/Aspect/Aspect_ColorScale.cxx
@@ -506,6 +506,7 @@ void Aspect_ColorScale::DrawScale( const Quantity_Color& aBgColor,
     case Aspect_TOCSP_RIGHT:
       x += colorWidth + spacer;
       break;
+	default: /* Do nothing */ break;
     }
     while ( i2 - i1 >= filter || ( i2 == 0 && i1 == 0 ) ) {
       Standard_Integer pos1 = i1;

src/BRepExtrema/BRepExtrema_DistShapeShape.cxx
@@ -455,6 +455,7 @@ void BRepExtrema_DistShapeShape::Dump(Standard_OStream& o) const
         ParOnFaceS1(i,r1,r2);
         o << "parameters on the first face :  u= " << r1 << " v=" <<  r2 << endl;
         break;
+	  default: /* Do nothing */ break;
     }
     switch (SupportTypeShape2(i))
     {
@@ -466,6 +467,7 @@ void BRepExtrema_DistShapeShape::Dump(Standard_OStream& o) const
         ParOnFaceS2(i,r1,r2);
         o << "parameters on the second face : u= " << r1 << " v=" <<  r2 << endl;
         break;
+	  default: /* Do nothing */ break;
     }
     o<<endl;
   } 

src/BRepExtrema/BRepExtrema_DistanceSS.cxx
@@ -527,6 +527,7 @@ void BRepExtrema_DistanceSS::Perform(const TopoDS_Shape& S1, const TopoDS_Shape&
           Perform( V1, F2, B1, B2 );
           break;
         }
+		default: /* Do nothing */ break;
       }
       break;
     }
@@ -567,6 +568,7 @@ void BRepExtrema_DistanceSS::Perform(const TopoDS_Shape& S1, const TopoDS_Shape&
           Perform( E1, F2, B1, B2 );
           break;
         }
+		default: /* Do nothing */ break;
       }
       break;
     }
@@ -599,9 +601,11 @@ void BRepExtrema_DistanceSS::Perform(const TopoDS_Shape& S1, const TopoDS_Shape&
           Perform( F1, F2, B1, B2 );
           break;
         }
+		default: /* Do nothing */ break;
       }
       break;
     }
+	default: /* Do nothing */ break;
   }
 }
 

src/ChFi3d/ChFi3d_Builder_0.cxx
@@ -1749,6 +1749,7 @@ TopAbs_Orientation ChFi3d_TrsfTrans(const IntSurf_TypeTrans T1)
   switch (T1)  {
     case IntSurf_In:  return TopAbs_FORWARD;
     case IntSurf_Out: return TopAbs_REVERSED;
+	default: /* Do nothing */ break;
   }
   return TopAbs_INTERNAL;
 }

src/DDocStd/DDocStd_ApplicationCommands.cxx
@@ -193,7 +193,7 @@ static Standard_Integer DDocStd_Open (Draw_Interpretor& di,
 	di << " could not retrieve , permission denied " << "\n" ;  
 	break;
       }
-	
+	  default: /* Do nothing */ break;
       }
       di << "DDocStd_Open : Error" << "\n";
     }	
@@ -255,6 +255,7 @@ static Standard_Integer DDocStd_SaveAs (Draw_Interpretor& di,
       case CDF_SS_Failure: {
 	di << " Write failure " << "\n" ;  
       }
+	  default: /* Do nothing */ break;
       }
       return 1;
     } else {

src/GeomAdaptor/GeomAdaptor_Surface.cxx
@@ -181,6 +181,7 @@ GeomAbs_Shape GeomAdaptor_Surface::UContinuity() const
         case GeomAbs_CN : return GeomAbs_CN; 
         case GeomAbs_C2 : return GeomAbs_C1; 
         case GeomAbs_C1 : return GeomAbs_C0;
+		default: /* Do nothing */ break;
       }
       Standard_NoSuchObject::Raise("GeomAdaptor_Surface::UContinuity");
 	  break;
@@ -192,6 +193,7 @@ GeomAbs_Shape GeomAdaptor_Surface::UContinuity() const
       return GC.Continuity();
     }
 	case GeomAbs_OtherSurface: Standard_NoSuchObject::Raise("GeomAdaptor_Surface::UContinuity");
+	default: /* Do nothing */ break;
   }
   return GeomAbs_CN;
 }
@@ -222,6 +224,7 @@ GeomAbs_Shape GeomAdaptor_Surface::VContinuity() const
         case GeomAbs_CN : return GeomAbs_CN; 
         case GeomAbs_C2 : return GeomAbs_C1;
         case GeomAbs_C1 : return GeomAbs_C0;
+		default: /* Do nothing */ break;
       }
       Standard_NoSuchObject::Raise("GeomAdaptor_Surface::VContinuity");
       break;
@@ -233,6 +236,7 @@ GeomAbs_Shape GeomAdaptor_Surface::VContinuity() const
       return GC.Continuity();
     }
 	case GeomAbs_OtherSurface: Standard_NoSuchObject::Raise("GeomAdaptor_Surface::VContinuity");
+	default: /* Do nothing */ break;
   }
   return GeomAbs_CN;
 }
@@ -270,10 +274,12 @@ Standard_Integer GeomAdaptor_Surface::NbUIntervals(const GeomAbs_Shape S) const
         case GeomAbs_C0: BaseS = GeomAbs_C1; break;
         case GeomAbs_C1: BaseS = GeomAbs_C2; break;
         case GeomAbs_C2: BaseS = GeomAbs_C3; break;
+		default: /* Do nothing */ break;
       }
       GeomAdaptor_Surface Sur((*((Handle(Geom_OffsetSurface)*)&mySurface))->BasisSurface());
       return Sur.NbUIntervals(BaseS);
     }
+	default: /* Do nothing */ break;
   }
   return 1;
 }
@@ -311,10 +317,12 @@ Standard_Integer GeomAdaptor_Surface::NbVIntervals(const GeomAbs_Shape S) const
         case GeomAbs_C0: BaseS = GeomAbs_C1; break;
         case GeomAbs_C1: BaseS = GeomAbs_C2; break;
         case GeomAbs_C2: BaseS = GeomAbs_C3; break;
+		default: /* Do nothing */ break;
       }
       GeomAdaptor_Surface Sur((*((Handle(Geom_OffsetSurface)*)&mySurface))->BasisSurface());
       return Sur.NbVIntervals(BaseS);
 	}
+	default: /* Do nothing */ break;
   }
   return 1;
 }
@@ -359,11 +367,13 @@ void GeomAdaptor_Surface::UIntervals(TColStd_Array1OfReal& T, const GeomAbs_Shap
         case GeomAbs_C0: BaseS = GeomAbs_C1; break;
         case GeomAbs_C1: BaseS = GeomAbs_C2; break;
         case GeomAbs_C2: BaseS = GeomAbs_C3; break;
+		default: /* Do nothing */ break;
       }
       GeomAdaptor_Surface Sur((*((Handle(Geom_OffsetSurface)*)&mySurface))->BasisSurface());
       myNbUIntervals = Sur.NbUIntervals(BaseS);
       Sur.UIntervals(T, BaseS);
     }
+	default: /* Do nothing */ break;
   }
 
   T(T.Lower()) = myUFirst;
@@ -410,11 +420,13 @@ void GeomAdaptor_Surface::VIntervals(TColStd_Array1OfReal& T, const GeomAbs_Shap
         case GeomAbs_C0: BaseS = GeomAbs_C1; break;
         case GeomAbs_C1: BaseS = GeomAbs_C2; break;
         case GeomAbs_C2: BaseS = GeomAbs_C3; break;
+		default: /* Do nothing */ break;
       }
       GeomAdaptor_Surface Sur((*((Handle(Geom_OffsetSurface)*)&mySurface))->BasisSurface());
       myNbVIntervals = Sur.NbVIntervals(BaseS);
       Sur.VIntervals(T, BaseS);
     }
+	default: /* Do nothing */ break;
   }
   
   T(T.Lower()) = myVFirst;

commit 5d73a6188afdb7d084a44a617c3f65e0642fe321

Fix buid failure introduced by 82de4a4

src/DNaming/DNaming_TransformationDriver.cxx
@@ -306,6 +306,7 @@ static void CollectShapes(const TopoDS_Shape& SSh, TopoDS_Compound& C,
     }
     break;
   case TopAbs_VERTEX:
+  {
     const Handle(TNaming_NamedShape) aNS =  TNaming_Tool::NamedShape(SSh, theLab);
     if(!aNS.IsNull())
       if(SMap.Add(SSh)) {
@@ -313,6 +314,7 @@ static void CollectShapes(const TopoDS_Shape& SSh, TopoDS_Compound& C,
 //	if(isPrimitive)
 //	  TagMap.Bind(SSh, aNS->Label().Tag());
       }
+  }
     break;
   default: /* Do nothing */ break;
   }   