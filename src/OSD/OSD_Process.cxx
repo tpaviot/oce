2af583cba21735097c0016e0f2ce65fce569c034
Matthew Dempsky <matthew@dempsky.org>
2011-08-27 19:32:12 -0700

    pass NULL for timezone pointer to gettimeofday()

--- src/OSD/OSD_Process.cxx
+++ src/OSD/OSD_Process.cxx
@@ -54,15 +54,19 @@ Quantity_Date  OSD_Process::SystemDate(){
 Quantity_Date result;
 Standard_Integer month=0,day=0,year=0,hh=0,mn=0,ss=0;
 struct tm transfert;
+time_t secs;
 struct timeval tval;
-struct timezone tzone;
 int status;
 
- status = gettimeofday( &tval, &tzone );
+ status = gettimeofday( &tval, NULL );
  if (status == -1) myError.SetValue (errno, Iam, "GetSystem");
  else {
-  memcpy(&transfert, localtime((time_t *)&tval.tv_sec), sizeof(struct
-tm));
+  secs = tval.tv_sec;
+#ifdef HAVE_LOCALTIME_R
+  localtime_r(&secs, &transfert);
+#else
+  memcpy(&transfert, localtime(&secs), sizeof(struct tm));
+#endif
   month = transfert.tm_mon + 1;  // Add to January (month #1)
   day   = transfert.tm_mday;
   year  = transfert.tm_year;
