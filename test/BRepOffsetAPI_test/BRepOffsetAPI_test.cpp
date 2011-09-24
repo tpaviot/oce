4343f2b66e89bb9d31ce95f6182122c9dac4d8fe
tpaviot <tpaviot@gmail.com>
2011-09-24 18:54:10 +0200

    Added testOffsetBox

--- test/BRepOffsetAPI_test/BRepOffsetAPI_test.cpp
+++ test/BRepOffsetAPI_test/BRepOffsetAPI_test.cpp
@@ -1,12 +1,14 @@
 #include <BRepBuilderAPI_MakePolygon.hxx>
+#include <BRepPrimAPI_MakeBox.hxx>
 #include <BRepOffsetAPI_MakeEvolved.hxx>
+#include <BRepOffsetAPI_MakeOffsetShape.hxx>
 #include <gp_Pnt.hxx>
 #include <GeomAbs_JoinType.hxx>
+#include <TopoDS_Shape.hxx>
 #include <gtest/gtest.h>
 
 TEST(BRepOffsetAPITestSuite, testEvolvedShape)
 {
-    // create a box, mixing integers and floats
     BRepBuilderAPI_MakePolygon P;
     P.Add(gp_Pnt(0.,0.,0.));
     P.Add(gp_Pnt(200.,0.,0.));
@@ -17,6 +19,15 @@ TEST(BRepOffsetAPITestSuite, testEvolvedShape)
     BRepOffsetAPI_MakeEvolved S(P.Wire(),wprof.Wire(),GeomAbs_Arc,Standard_True,Standard_False,Standard_True,0.0001);
     S.Build();
     ASSERT_TRUE(S.IsDone());
+}
+
+TEST(BRepOffsetAPITestSuite, testOffsetBox)
+{
+    BRepPrimAPI_MakeBox b(gp_Pnt(-100,-60,-80),150,200,170);
+    b.Build();
+    TopoDS_Shape b_shape = b.Shape();
+    BRepOffsetAPI_MakeOffsetShape offsetA(b_shape,60,0.01);
+    ASSERT_TRUE(offsetA.IsDone());
 }
 
 int main(int argc, char **argv){
