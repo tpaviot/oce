17a37c040b2e280c5f2632bea4b95193d2c5d6ae
tpaviot <tpaviot@gmail.com>
2011-10-21 05:36:32 +0200

    Explicitely specify 64bit UndefinedAdress as long long unsigned int

--- inc/Handle_Standard_Transient.hxx
+++ inc/Handle_Standard_Transient.hxx
@@ -21,7 +21,7 @@
 
 #ifndef UndefinedHandleAddress 
 #ifdef _OCC64
-#define UndefinedHandleAddress ((Standard_Transient *)0xfefdfefdfefd0000)
+#define UndefinedHandleAddress ((Standard_Transient *)0xfefdfefdfefd0000llu)
 #else
 #define UndefinedHandleAddress ((Standard_Transient *)0xfefd0000)
 #endif
