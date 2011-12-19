bc4428d34372f9b524e9c5cf96c7ec1fa2d6972a
He Yuqi <yuqi.he@gmail.com>
2011-12-19 20:18:45 +0800

    Test for importing a null swept curve.

--- test/STEPImport_test/STEPImport_test.cpp
+++ test/STEPImport_test/STEPImport_test.cpp
@@ -100,6 +100,38 @@ TEST(STEPImportTestSuite, testImportAP214_3)
         }
 }
 
+//------------------------------------------------------------------------------
+// Test for importing a surface of linear extrusion with a null swept curve.
+// Success if no exceptions (C++ exceptions and Windows SEH exceptions) raised.
+//------------------------------------------------------------------------------
+TEST(STEPImportTestSuite, testImportNullSweptCurve) {
+  Standard_CString aFileName = (Standard_CString) step_file_null_swept_curve;
+  STEPControl_Reader aReader;
+
+  IFSelect_ReturnStatus status = aReader.ReadFile (aFileName);
+  if (status != IFSelect_RetDone) {
+    return ;
+  }
+
+  int nb_root   = aReader.NbRootsForTransfer ();
+  int nb_shapes = 0;
+
+  for (Standard_Integer n = 1; n <= nb_root; ++n) {
+    Standard_Boolean result = aReader.TransferRoot (n);
+    if (! result) {
+      continue;
+    }
+
+    nb_shapes = aReader.NbShapes ();
+    if (nb_shapes > 0) {
+      TopoDS_Shape shape = aReader.Shape (n);
+      if (shape.IsNull ()) {
+        continue;
+      }
+    }
+  }
+}
+
 int main(int argc, char **argv){
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
--- test/STEPImport_test/STEPImport_test_config.h.cmake
+++ test/STEPImport_test/STEPImport_test_config.h.cmake
@@ -6,5 +6,6 @@
 #cmakedefine step_file_ap214_1 "@step_file_ap214_1@"
 #cmakedefine step_file_ap214_2 "@step_file_ap214_2@"
 #cmakedefine step_file_ap214_3 "@step_file_ap214_3@"
+#cmakedefine step_file_null_swept_curve "@step_file_null_swept_curve@"
  
 #endif
