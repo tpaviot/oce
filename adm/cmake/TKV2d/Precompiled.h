df461e6593bd94979a3a73b78e8a2c0d9773e6de
Denis Barbier <bouzim@gmail.com>
2013-04-26 23:13:45 +0200

    Upgrade source and dependencies in adm/cmake/*/CMakeLists.txt
    TKV2d had been removed.

--- adm/cmake/TKV2d/Precompiled.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "../TKService/Precompiled.h"
-#include "../TKTopAlgo/Precompiled.h"
-#include "../TKHLR/Precompiled.h"
-
-#include "AIS2D.hxx"
-#include "Graphic2d_View.hxx"
-#include "V2d.hxx"
