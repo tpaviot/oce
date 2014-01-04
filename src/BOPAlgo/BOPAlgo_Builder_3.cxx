tpaviot/oce

commit 8646fb31516b11489eb7c0ec5f65bd25df655d73

[warning-fix][unused-local-var]

src/BOPAlgo/BOPAlgo_Builder_3.cxx
@@ -166,7 +166,7 @@ void BOPAlgo_Builder::FillIn3DParts(BOPCol_DataMapOfShapeListOfShape& theInParts
 				    const BOPCol_BaseAllocator& )
 {
   Standard_Boolean bHasImage;
-  Standard_Integer i, k, aNbS, aNbLIF, nFP, aNbFP, aNbFIN, iIsIN;
+  Standard_Integer i, k, aNbS, aNbLIF, nFP, aNbFIN, iIsIN;
   TopoDS_Solid aSD;
   TopoDS_Iterator aIt;
   BRep_Builder aBB; 
@@ -298,8 +298,6 @@ void BOPAlgo_Builder::FillIn3DParts(BOPCol_DataMapOfShapeListOfShape& theInParts
     // 2.6. Select boxes of faces that are not out of aBoxS
     aSelector.Clear();
     aSelector.SetBox(aBoxS);
-    //
-    aNbFP=aBBTree.Select(aSelector);
     //
     const BOPCol_ListOfInteger& aLIFP=aSelector.Indices();
     //
@@ -474,7 +472,7 @@ void BOPAlgo_Builder::BuildSplitSolids(BOPCol_DataMapOfShapeListOfShape& theInPa
   myErrorStatus=0;
   //
   Standard_Boolean bFlagSD;
-  Standard_Integer i, aNbS, iErr, aNbSFS;
+  Standard_Integer i, aNbS, iErr;
   TopExp_Explorer aExp;
   BOPCol_ListIteratorOfListOfShape aIt;
   BOPCol_DataMapIteratorOfDataMapOfShapeShape aIt1;
@@ -544,8 +542,6 @@ void BOPAlgo_Builder::BuildSplitSolids(BOPCol_DataMapOfShapeListOfShape& theInPa
       aSFS.Append(aF);
     }
     //
-    aNbSFS=aSFS.Extent();
-    //
     // 1.3 Build new solids   
     Handle(NCollection_IncAllocator) aAlr1;
     aAlr1=new NCollection_IncAllocator();  
@@ -600,7 +596,7 @@ void BOPAlgo_Builder::FillInternalShapes()
 {
   myErrorStatus=0;
   //
-  Standard_Integer i, j,  aNbS, aNbSI, aNbSx, aNbSd;
+  Standard_Integer i, j,  aNbS, aNbSI, aNbSx;
   TopAbs_ShapeEnum aType;
   TopAbs_State aState; 
   TopoDS_Iterator aItS;
@@ -730,8 +726,6 @@ void BOPAlgo_Builder::FillInternalShapes()
       }
     }
   }// for (i=0; i<aNbS; ++i) {
-  //
-  aNbSd=aLSd.Extent();
   //
   // 3. Some shapes of aMSI can be already tied with faces of 
   //    split solids

src/AIS/AIS_Dimension.cxx
@@ -1142,7 +1142,6 @@ Standard_Boolean AIS_Dimension::InitCircularDimension (const TopoDS_Shape& theSh
         theCircle = aMkCirc.Value()->Circ();
       }
 
-      gp_Vec aVec = gp_Vec (theCircle.Location(), aCurPos).Normalized();
       aFirstPoint = ElCLib::Value (aFirstU, theCircle);
       aLastPoint = ElCLib::Value (aLastU,  theCircle);
     }

src/AdvApp2Var/AdvApp2Var_MathBase.cxx
@@ -7098,10 +7098,6 @@ int mmpojac_(doublereal *tparam,
 {
   integer c__2 = 2;
   
-    /* Initialized data */
-
-    integer nbcof = -1;
-
     /* System generated locals */
     integer valjac_dim1, i__1, i__2;
 
@@ -7212,8 +7208,6 @@ int mmpojac_(doublereal *tparam,
 	tnorm[ii - 1] = sqrt(aux2 * (kk1 * 2. + (*iordre << 1) + 1) / pow__ii(&
 		c__2, &i__2));
     }
-
-    nbcof = *ncoeff;
 
 /*      END IF */
 
@@ -9885,7 +9879,6 @@ L9999:
   logical colin;
   doublereal valaux;
   integer aux;
-  logical nul;
  
 /* ***********************************************************************
  */
@@ -9953,7 +9946,6 @@ L9999:
     if (*ndimen <= 1 || *ndimen > 3) {
 	goto L9101;
     }
-    nul = FALSE_;
     d__ = 1;
     aux = 0;
     while(d__ <= *ndimen) {

src/AdvApp2Var/AdvApp2Var_SysBase.cxx
@@ -155,9 +155,6 @@ int AdvApp2Var_SysBase::macinit_(integer *imode,
 
 {
  
-  /* Fortran I/O blocks */
-  cilist io______1 = { 0, 0, 0, (char*) "(' --- Debug-mode : ',I10,' ---')", 0 };
-  
   /* ************************************************************************/
   /*     FUNCTION : */
   /*     ---------- */
@@ -737,7 +734,7 @@ int macrgfl_(intptr_t *iadfld,
   integer ibid, ienr;
   doublereal* t = 0;
   integer novfl = 0;
-  intptr_t ioff,iadrfl, iadt;
+  intptr_t ioff,iadt;
   
   
   /* ***********************************************************************
@@ -875,15 +872,9 @@ int macrmsg_(const char *,//crout,
 {
   
   /* Local variables */
-  integer inum, iunite;
+  integer inum;
   char cfm[80], cln[3];
   
-  /* Fortran I/O blocks */
-  cilist io___5 = { 0, 0, 0, cfm, 0 };
-  cilist io___6 = { 0, 0, 0, cfm, 0 };
-  cilist io___7 = { 0, 0, 0, cfm, 0 };
- 
-
 /* ***********************************************************************
  */
 
@@ -2727,7 +2718,7 @@ int AdvApp2Var_SysBase::mcrrqst_(integer *iunit,
   doublereal dfmt;
   integer ifmt, iver;
   char subr[7];
-  integer ksys , ibyte, irest, isyst, ier;
+  integer ksys , ibyte, irest, ier;
   intptr_t iadfd, iadff, iaddr,lofset, loc;
   integer izu;
 
@@ -3260,9 +3251,6 @@ int AdvApp2Var_SysBase::mswrdbg_(const char *,//ctexte,
 				 ftnlen )//ctexte_len)
 
 {
-
-  cilist io___1 = { 0, 0, 0, 0, 0 };
-  
 
 /* ***********************************************************************
  */

src/BOPAlgo/BOPAlgo_BuilderSolid.cxx
@@ -233,7 +233,7 @@ static
   //
   myLoops.Clear();
   //
-  Standard_Integer aNbLF, aNbOff, aNbFP, aNbFA;
+  Standard_Integer aNbLF, aNbOff, aNbFP;
   Standard_Integer i;
   TopAbs_Orientation anOr;
   TopoDS_Edge aEL;
@@ -402,8 +402,6 @@ static
   //
   aEFMap.Clear();
   AddedFacesMap.Clear();
-  //
-  aNbFA=myShapesToAvoid.Extent();
   //
   aItM.Initialize(myShapesToAvoid);
   for (; aItM.More(); aItM.Next()) {

src/BOPAlgo/BOPAlgo_PaveFiller_1.cxx
@@ -44,7 +44,7 @@
   void BOPAlgo_PaveFiller::PerformVV() 
 {
   Standard_Boolean bWithSubShape;
-  Standard_Integer n1, n2, iFlag, nX, n, aSize, i, aNbVV, j, iX, k, aNbBlocks;
+  Standard_Integer n1, n2, iFlag, nX, n, aSize, i, j, iX, k, aNbBlocks;
   Handle(NCollection_IncAllocator) aAllocator;
   BOPCol_DataMapIteratorOfDataMapOfIntegerListOfInteger aItMILI;
   BOPCol_ListIteratorOfListOfInteger aItLI, aItLI2;
@@ -131,7 +131,6 @@
       }
     }
   }
-  aNbVV=aVVs.Extent();
   //
   //-----------------------------------------------------scope t
   aLV.Clear();

src/BOPAlgo/BOPAlgo_PaveFiller_6.cxx
@@ -271,7 +271,7 @@ void BOPAlgo_PaveFiller::MakeBlocks()
   Standard_Boolean bExist, bValid2D;
   Standard_Integer i, nF1, nF2, aNbC, aNbP, j;
   Standard_Integer nV1, nV2;
-  Standard_Real aTolR3D, aTolR2D, aT1, aT2, aTol;
+  Standard_Real aTolR3D, aT1, aT2, aTol;
   Handle(NCollection_IncAllocator) aAllocator;
   BOPDS_ListIteratorOfListOfPaveBlock aItLPB;
   TopoDS_Edge aES;
@@ -310,7 +310,6 @@ void BOPAlgo_PaveFiller::MakeBlocks()
     const TopoDS_Face& aF2=(*(TopoDS_Face *)(&myDS->Shape(nF2)));
     //
     aTolR3D=aFF.TolR3D();
-    aTolR2D=aFF.TolR2D();
     //
     // Update face info
     if (aMF.Add(nF1)) {

src/BOPAlgo/BOPAlgo_PaveFiller_7.cxx
@@ -72,7 +72,7 @@ static void UpdateVertices(const TopoDS_Edge& aE,
   }
   //
   Standard_Boolean bCB, bV1, bV2;
-  Standard_Integer i, nE, nV1, nV2, nSp, aNbPB, nOrE;
+  Standard_Integer i, nE, nV1, nV2, nSp, aNbPB;
   Standard_Real aT1, aT2;
   Handle(NCollection_IncAllocator) aAllocator;
   BOPDS_ListIteratorOfListOfPaveBlock aItPB, aItPBCB;
@@ -90,7 +90,6 @@ static void UpdateVertices(const TopoDS_Edge& aE,
     //DEBf
     if (aNbPB) {
       aPBx=aLPB.First();
-      nOrE=aPBx->OriginalEdge();
     }
     //DEBt
     if (aNbPB==1) {

commit 64f689068aab9b94126376554adc0e9223407990

Fix bug introduced in 8646fb3
Many boolean operations did not behave as expected.

src/BOPAlgo/BOPAlgo_Builder_3.cxx
@@ -299,6 +299,8 @@ void BOPAlgo_Builder::FillIn3DParts(BOPCol_DataMapOfShapeListOfShape& theInParts
     aSelector.Clear();
     aSelector.SetBox(aBoxS);
     //
+    aBBTree.Select(aSelector);
+    //
     const BOPCol_ListOfInteger& aLIFP=aSelector.Indices();
     //
     // 2.7. Collect faces that are IN aSolid [ aLFIN ]