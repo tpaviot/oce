8ef49a1777c97e3408d23afcdd4f15188125cc04
QbProg <tholag@gmail.com>
2014-01-11 15:45:10 +0100

    Small additions to main precompiled file

--- adm/cmake/TKernel/Precompiled.h
+++ adm/cmake/TKernel/Precompiled.h
@@ -3,5 +3,19 @@
 #include <string>
 #include <iostream>
 #include <math.h>
+
 #include "Standard.hxx"
 #include "Standard_PrimitiveTypes.hxx"
+#include "Standard_Stream.hxx"
+#include "Standard_Failure.hxx"
+#include "Standard_Transient.hxx"
+
+#ifdef USE_STL_STREAM
+#include <sstream>
+#else /* USE_STL_STREAM */
+#ifdef WNT
+#include <strstrea.h>
+#else
+#include <strstream.h>
+#endif
+#endif
