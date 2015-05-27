c978420377c65511ceac0d070dbb9e14fc31a779
Julien Finet <julien.finet@kitware.com>
2015-05-27 23:18:51 +0200

    Fix TKIVtk Precompiled.h

--- adm/cmake/TKIVtk/Precompiled.h
+++ adm/cmake/TKIVtk/Precompiled.h
@@ -1,12 +1,7 @@
 #pragma once
-#include "../TKernel"
-#include "../TKBRep"
-#include "../TKG2d"
-#include "../TKG3d"
-#include "../TKGeomAlgo"
-#include "../TKGeomBase"
-#include "../TKMath"
-#include "../TKMesh"
-#include "../TKService"
-#include "../TKTopAlgo"
-#include "../TKV3d"
+
+#include <string>
+#include <iostream>
+#include <math.h>
+
+#include "Standard.hxx"
