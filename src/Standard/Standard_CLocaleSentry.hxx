7947800ef1b18b227d05401389cf7dbbd788bff4
Denis Barbier <bouzim@gmail.com>
2015-10-05 20:46:06 -0400

    ports/mingw-locale
    ===================================================================

--- src/Standard/Standard_CLocaleSentry.hxx
+++ src/Standard/Standard_CLocaleSentry.hxx
@@ -65,7 +65,7 @@ public:
 
 #ifdef HAVE_XLOCALE_H
   typedef  locale_t clocale_t;
-#elif defined(_WIN32) && !defined(__BORLANDC__)
+#elif defined(_WIN32) && !defined(__BORLANDC__) && !defined(__MINGW32__)
   typedef _locale_t clocale_t;
 #else
   typedef void*     clocale_t;
@@ -78,7 +78,7 @@ public:
 private:
 
   void* myPrevLocale;       //!< previous locale, platform-dependent pointer!
-#if defined(_WIN32) && !defined(__BORLANDC__)
+#if defined(_WIN32) && !defined(__BORLANDC__) && !defined(__MINGW32__)
   int   myPrevTLocaleState; //!< previous thread-locale state, MSVCRT-specific
 #endif
 
