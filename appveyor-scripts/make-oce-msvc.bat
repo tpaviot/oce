8a6c75218c09fa20f7a61b7b611664b8cd92eb92
Thomas Paviot <tpaviot@gmail.com>
2018-02-13 08:25:09 +0100

    Updated ci (travis, appveyor) to upstream 7.2.0

--- appveyor-scripts/make-oce-msvc.bat
+++ appveyor-scripts/make-oce-msvc.bat
@@ -1,11 +1,15 @@
 cd C:\projects\oce
 mkdir cmake-build
 cd cmake-build
-cmake -DOCE_VISUALISATION:BOOL=ON -DOCE_DATAEXCHANGE:BOOL=ON -DOCE_OCAF:BOOL=ON ^
-      -DOCE_WITH_GL2PS:BOOL=OFF ^
-      -DOCE_WITH_FREEIMAGE:BOOL=OFF ^
-      -DOCE_TESTING:BOOL=OFF ^
-      -DOCE_MULTITHREAD_LIBRARY=NONE ^
+cmake -DBUILD_WITH_DEBUG:BOOL=OFF ^
+      -DCMAKE_BUILD_TYPE:STRING=Release ^
+      -DBUILD_Inspector:BOOL=OFF ^
+      -DUSE_TCL:BOOL=OFF ^
+      -DUSE_GL2PS:BOOL=OFF ^
+      -DUSE_FREEIMAGE:BOOL=OFF ^
+      -DUSE_TBB:BOOL=OFF ^
+      -DUSE_FFMPEG:BOOL=OFF ^
+      -DUSE_VTK:BOOL=OFF ^
       -DOCE_INSTALL_PREFIX=C:\oce-%oce_version% ^
       -G "%generator%" ..
 msbuild /m:4 /verbosity:quiet /p:Configuration=%configuration% oce.sln
--- appveyor-scripts/make-oce-msys.bat
+++ appveyor-scripts/make-oce-msys.bat
@@ -1,14 +1,16 @@
 cd C:\projects\oce
 mkdir cmake-build
 cd cmake-build
-cmake -DOCE_VISUALISATION:BOOL=ON ^
-      -DOCE_DATAEXCHANGE:BOOL=OFF ^
-      -DOCE_OCAF:BOOL=OFF ^
-      -DOCE_WITH_GL2PS:BOOL=ON ^
-      -DOCE_WITH_FREEIMAGE:BOOL=OFF ^
-      -DOCE_TESTING:BOOL=ON ^
-      -DOCE_COPY_HEADERS_BUILD:BOOL=ON ^
-      -DOCE_INSTALL_PREFIX=C:\oce-0.18.3 ^
+cmake -DBUILD_WITH_DEBUG:BOOL=OFF ^
+      -DCMAKE_BUILD_TYPE:STRING=Release ^
+      -DBUILD_Inspector:BOOL=OFF ^
+      -DUSE_TCL:BOOL=OFF ^
+      -DUSE_GL2PS:BOOL=OFF ^
+      -DUSE_FREEIMAGE:BOOL=OFF ^
+      -DUSE_TBB:BOOL=OFF ^
+      -DUSE_FFMPEG:BOOL=OFF ^
+      -DUSE_VTK:BOOL=OFF ^
+      -DOCE_INSTALL_PREFIX=C:\oce-%oce_version% ^
       -G "%generator%" ..
 mingw32-make -j5
 mingw32-make install
