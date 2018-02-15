0cf3276dc6356cfafb77464cba8a55027fbaece3
Thomas Paviot <tpaviot@gmail.com>
2018-02-15 08:23:13 +0100

    Udated FetchBundle batch script

--- FetchBundle.bat
+++ FetchBundle.bat
@@ -1,12 +1,5 @@
 @echo off
-if "%1"=="/help" goto help
-if "%1"=="--help" goto help
-git clone http://github.com/QbProg/oce-win-bundle
-if "%1"=="" goto end
+git clone http://github.com/tpaviot/oce-win-bundle
 cd oce-win-bundle
-git checkout oce-win-bundle-%1
+git checkout oce-0.19
 cd ..
-goto end
-:help
-echo "Usage : FetchBundle.bat <version>"
-:end
