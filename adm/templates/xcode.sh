2cc3e117e8ec62407061cc67dff9e2003b56f345
tpaviot <tpaviot@gmail.com>
2020-09-16 08:09:06 +0200

    removed all

--- adm/templates/xcode.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export TARGET="xcd"
-
-source ./env.sh "$1" "$TARGET"
-
-open -a Xcode ./adm/mac/xcd/OCCT.xcworkspace
--- samples/qt/FuncDemo/custom.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-rem Define QTDIR variable
-set QTDIR=
